ebc55d646ba7d34ccf17680fa16257edb39aedc1
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-01-21 13:01:31 -0500

    updates to components

---
+++ src/content/Landing/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Landing = props => {
+    return (
+        <div></div>
+    )
+};
--- src/content/index.js
+++ src/content/index.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import '../styling/style.css';
+
+// export const is quicker but must be imported directly
+// rather than as a default
+// refer to index.js at src root to see how import works
 export const App = () => {
     return (
+        // Router for setting routes
         <Router>
             <div className=''>
             </div>
