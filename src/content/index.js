38645cf6105e0d3c6b2c89f1fb28f724e21539f8
Blake DeGraw <bbdegraw@gmail.com>
2021-02-10 14:49:14 -0800

    able to fetch API data

--- src/content/index.js
+++ src/content/index.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import { Display } from './Display';
 import { Landing } from './Landing';
-
 import '../styling/style.css';
 
-// export const is quicker but must be imported directly
-// rather than as a default
-// refer to index.js at src root to see how import works
-export const App = () => {
+function App () {
+
+    const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey='
+    const API_KEY = process.env.REACT_APP_API_KEY
+
+    useEffect(() => {
+        fetch(url+API_KEY)
+        .then(res => res.json())
+        .then(resData => {
+            resData = Object.values(resData)
+            console.log(resData)
+        })
+    })
+
     return (
-        // Router for setting routes
-        <Router>
-            <div className='app'>
-                <Landing />
-                <Display />
-            </div>
-        </Router>
+        <div>
+            <h1>hi</h1>
+        </div>
+        // <Router>
+        //     <div className='app'>
+        //         <Landing />
+        //         <Display />
+        //     </div>
+        // </Router>
     )
-};
+}
+
+export default App
