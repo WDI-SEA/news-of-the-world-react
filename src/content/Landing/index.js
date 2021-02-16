15e6be580823642e3935e5a7b55bb0377175a232
unknown <dancesfire@gmail.com>
2021-02-15 18:46:21 -0800

    display page works, rest of site waiting on unemployment

--- src/content/Landing/index.js
+++ src/content/Landing/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios'
+
 
 import { Link } from 'react'
 
@@ -9,20 +9,7 @@ import { Display } from '../Display';
 
 export const Landing = props => {
 
-const [news, setNews] = useState([])
 
-
-    useEffect(() => {
-        axios('http://newsapi.org/v2/top-headlines?country=us&apiKey=63ca6b792d99475da52089c83360b842')
-        // .then(response => response.json())
-        .then(rdata => {
-            console.log(rdata)
-            // rdata = Object.values(rdata) 
-            console.log(rdata)
-            setNews(rdata)
-            
-          })
-        }, [])
 
 
     //    news.map((stories, index) => {
@@ -33,7 +20,7 @@ const [news, setNews] = useState([])
         <div>
             <h1>
                 Welcome to news of the world! Please click to see it! 
-                <Link to={'./Display/index'}>{Display}</Link>
+                {/* <Link to={'./Display/index'}>{Display}</Link> */}
             </h1>
         </div>
     )
