6070a13529eb248569f149af009f4c31568eac65
isaac8069 <isaac8069@gmail.com>
2021-12-06 17:54:43 -0500

    updated App js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,58 @@
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
+import { useEffect, useState } from 'react'
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
+import Article from './pages/Article';
 
 function App() {
+  let [searchResults, setSearchResults] = useState([])
+  let [searchTerm, setSearchTerm] = useState('bitcoin')
+  let [faves, setFaves] = useState([])
+
+  useEffect(()=>{
+      fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=6935862978ef427e84ba0add779331d7`)
+      .then(response => response.json())
+      .then(rdata => {
+          console.log('response data', rdata)
+          setSearchResults(rdata.articles)
+      })
+  }, [searchTerm])
+
+  
+  const handleChange = (e) => {
+      setSearchTerm(e.target.value)
+  }
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      setSearchTerm(e.target[0].value)
+      console.log(e)
+  }
+  
+  const handleClick = (article) => {
+      if (faves.indexOf(article)===-1) {
+          setFaves([...faves, article])
+        }
+      }
+
+  const allResults = searchResults.map((article, i) =>{
+      return <li onClick={()=>handleClick(article)} key={i}>{article.title}</li>
+  })
+
+  const allFaves = faves.map((article, i) =>{
+      return <li key={i}>{article.title}</li>   
+  })
+
+
+
   return (
     <div className="App">
       <main>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<Landing allResults={allResults} allFaves={allFaves} handleSubmit={handleSubmit}/>} />
           <Route path="/display" element={<Display />} />
+          {/* <Route path="/article" element={<Article myFave={myFave}/>} /> */}
         </Routes>
       </main>
     </div>
---
+++ src/components/pages/Article.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
+
+// const Article = props => {
+    
+//         const myFave = props.articles.map((article, i)=>{
+//             return <li><Link to={`/pages/article/${i}`}>{article.title}</Link></li>
+//         })
+//         return (
+//             <div class="container">
+//                 <h1>Favorite Articles</h1>
+//                 <ul>
+//                     {myFave}
+//                 </ul>
+//             </div>
+//         )
+// }
+
+// export default Article
