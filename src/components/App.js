c0a47bfd82801d6d2fc410d7e3063a9be7e16825
jmeier2715 <jmeier2715@gmail.com>
2021-12-07 09:09:44 -0500

    need help w adding key

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,81 @@
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
-
+import { useEffect, useState } from "react";
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <main>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/display" element={<Display />} />
-        </Routes>
-      </main>
+  let[data, setData] = useState([])
+  let[search, setSearch] = useState('')
+  let[faves, setFaves] = useState ([])
+
+  let apiURL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a6cc729078714e6992b6effcda0e415b"
+  if(search !== "") {
+  apiURL = `https://newsapi.org/v2/everything?q=${search}&apiKey=a6cc729078714e6992b6effcda0e415b`
+}
+useEffect(() => {
+  fetch(apiURL)
+  .then(response => response.json())
+  .then(rdata => {
+    console.log(rdata.articles)
+    setData(rdata.articles)
+  })
+}, [search])
+
+const handleChange = (e) => {
+  e.preventDefault()
+  setSearch(e.target.value)
+}
+
+const getFilteredArticles = () => {
+  return data.filter((a) => {
+    return a.title.toLowerCase().includes(search.toLowerCase())
+  })
+}
+
+const handleClick = (article) => {
+  if (faves.indexOf(article)===-1){
+    setFaves([...faves, article])
+    console.log('added', faves)
+  }
+}
+
+let articleFavorites = faves
+console.log ('these', articleFavorites)
+
+return (
+  <div className="App">
+    <div>
+      <label htmlFor="article-search">Search: </label>
+      <input 
+        type="text"
+        id="article-search"
+        value={search}
+        onChange={handleChange}
+      />
     </div>
-  );
+    <main>
+      <Routes>
+        <Route path="/"
+        element={<Landing 
+          articles={data}
+          handleClick={handleClick}
+          faves={articleFavorites}/>}
+          value={faves} />
+        <Route path="/:id" element={<Landing articles={getFilteredArticles()} />} />
+        <Route path="/display" element={<Display />} />
+      </Routes>
+    </main>
+</div>
+);
 }
 
 export default App;
+
+
+
+
+
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-function Landing() {
+function Landing(props) {
+    const allArticles = props.articles.map(v=>{
+        return (
+            <li onClick={()=>props.handleClick(v.title)}>
+                {v.title}
+            </li>
+        )
+    })
+
+        const allFaves = props.faves.map((v, index)=>{
+            return (
+                <li key={index}>
+                    {v.faves}
+                </li>
+            )
+        })
+    console.log('array', allArticles)
     return (
+        <div>
         <h1>Landing Page</h1>
+            <ul>{allArticles}</ul>
+        
+        <h1>Favorite Articles</h1>
+            <ul>{allFaves}</ul>
+        </div>
     )
 }
+
 
 export default Landing;
