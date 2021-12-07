8a823d107158233e42699126fb59c96406113b0d
Derrick Garcon <dgarconpro@gmail.com>
2021-12-07 09:11:18 -0500

    completed deliverable

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React from "react"
+import { Link } from "react-router-dom"
+import "../../App.css"
 
-function Landing() {
+function Landing(props) {
+  console.log("this is props\n", props)
+  const articleList = props.articles.map((a, i) => {
     return (
-        <h1>Landing Page</h1>
+      <li key={i} className="articleContainer">
+        <h3 className="title">
+          <Link to={`/display/${i}`}>{a.title}</Link>
+        </h3>
+        {/* <small>Written by: {a.author}</small> */}
+        {/* <img src={a.urlToImage} alt={a.title} className='img' /> */}
+      </li>
     )
+  })
+
+  const allFaves = props.faves.map((f, i) => {
+    return (
+      <li key={i}>
+        <h3>
+          <Link to={`/display/${i}`}>{f.title}</Link>
+        </h3>
+      </li>
+    )
+  })
+
+  return (
+    <div>
+      <div className="searchContainer">
+        <label htmlFor="articleSearch">Search for an article: </label>
+        <input
+          id="articleSearch"
+          type="text"
+          value={props.search}
+          onChange={props.handleChange}
+          className="searchInput"
+        />
+      </div>
+      <ul className="listContainer">{articleList}</ul>
+      <div>
+        <h1>Here are your saved articles: </h1>
+        <ul className="favContainer">{allFaves}</ul>
+      </div>
+    </div>
+  )
 }
 
-export default Landing;
+export default Landing
