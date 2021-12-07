02edbd0f41ae572ca9da9818e9a82c3f577d716b
Mackmiller <mackmiller19@gmail.com>
2021-12-06 21:40:18 -0500

    MPV deliverable with pseudo code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,58 @@
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
-
+import React, { useState, useEffect } from 'react'
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
 function App() {
+
+  let [data, setData] = useState({articles:[]})
+  let [search, setSearch] = useState('')
+  let [favorites, setFavorites] = useState([])
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/everything?q=crypto&apiKey=d05ca0f3400c4ad2b77ef7b0b09998f3`)
+    .then(res => res.json())
+    .then(responseData => {
+      responseData = Object.values(responseData)
+      //console.log('this is response data', responseData[2])
+      //set data useState to articles found in data array element 2
+      setData({articles: responseData[2]})
+    })
+  }, [])
+
+  //account for input box to change via user typing
+  const handleChange = (e) => {
+    //set search useState to whatever user has typed in box (value)
+    setSearch(e.target.value)
+  }
+
+  //filter articles by search term
+  const getFilteredArticles = () => {
+    //change user input to all lowercase
+    let searchTerm = search.toLowerCase()
+    return data.articles.filter(article => {
+      //change data titles to all lowercase
+      let lowercaseArticle = article.title.toLowerCase()
+      //return data titles that include the search term (current search useState)
+      return lowercaseArticle.includes(searchTerm)
+    })
+  }
+
+  //account for button click via user click
+  const handleClick = (article) => {
+    //add article to existing faves array
+    setFavorites([...favorites, article])
+  }  
+
   return (
     <div className="App">
       <main>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/display" element={<Display />} />
+          {/* landing */}
+          <Route path="/" element={<Landing articles={getFilteredArticles()} search={search} handleChange={handleChange} favorites={favorites} />} />
+          {/* display id */}
+          <Route path="/display/:id" element={<Display articles={data.articles} handleClick={handleClick} />} />
         </Routes>
       </main>
     </div>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,8 +1,54 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
-function Landing() {
+function Landing(props) {
+    //test for props brought in from App parent
+    //console.log('this is props:', props)
+    //return list of articles
+    const articleList = props.articles.map((article, i) => {
+        return (
+            //assign each article list item a key, i
+            <li key={i}>
+                {/* link up display route to show one article list item, i */}
+                <h3><Link to = {`/display/${i}`}>{article.title}</Link></h3>
+                {/* author */}
+                <small>Written by: {article.author}</small>
+            </li>
+        )
+    })
+    //return list of favorited articles, similar to above
+    const allFavorites = props.favorites.map((favorite, i) => {
+        return (
+            <li key={i}>
+                {/* link straight to article url source */}
+                <h3><a href={favorite.url}>{favorite.title}</a></h3>
+            </li>
+        )
+    })
     return (
-        <h1>Landing Page</h1>
+        //basic label and input box for user to type search terms
+        //populated articles list
+        //populated favorited articles list
+        <div>
+            <div>
+                <label htmlFor='articleSearch'>Search for an article: </label>
+                <input
+                    id='articleSearch'
+                    type='text'
+                    value={props.search}
+                    onChange={props.handleChange}
+                />
+            </div>
+            <ul>
+                {articleList}
+            </ul>
+            <div>
+                <h1>Saved articles: </h1>
+                <ul>
+                    {allFavorites}
+                </ul>
+            </div>
+        </div>
     )
 }
 
