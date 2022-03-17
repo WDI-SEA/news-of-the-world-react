9e19f6505f78d36ccc1d13daf4729ba67181e123
taekim90 <taehk15@gmail.com>
2022-03-16 20:42:05 -0700

    start of news of the world

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,55 @@
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
+import React, { useState, useEffect } from 'react'
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
+
 function App() {
+  const [newsInfo, setNewsInfo] = useState([])
+
+  const [search, setSearch] = useState('')
+
+  // const [newsUrl, setNewsUrl] = useState('')
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_NEWS_API_KEY
+    const newsUrl = (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`)
+    console.log("UseEffect is firing!")
+    // const req = new Request(newsUrl);
+    fetch(newsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData.articles)
+      setNewsInfo(jsonData.articles)
+    })
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    console.log('submit button pressed')
+  }
+
   return (
     <div className="App">
       <main>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/display" element={<Display />} />
+          <Route 
+            path="/" 
+            element={
+              <Landing 
+                newsInfo={newsInfo} 
+                search={search} 
+                handleChange={handleChange} 
+                handleSubmit={handleSubmit} 
+              />
+            }
+          />
+          <Route path="/display" element={<Display newsInfo={newsInfo} />} />
         </Routes>
       </main>
     </div>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,8 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Landing() {
+function Landing({ newsInfo, search, handleChange, handleSubmit }) {
+    const allArticles = newsInfo.map((article, index) => {
+        return (
+            <li key={`articlekey-${index}`}>
+                {/* <Link to ={`/display`}>{article.title}</Link> */}
+                <h2>{article.title}</h2>
+                <h3>{article.author}</h3>
+                {/* <p>{article.content}</p>
+                <p>{article.description}</p> */}
+            </li>
+        )
+    })
+
+    
     return (
-        <h1>Landing Page</h1>
+        <>
+            <h1>Landing Page</h1>
+            <form>
+                <h2>Search For An Article</h2>
+                <label htmlFor="searchbox"/>
+                <input 
+                    id="search" 
+                    type="text" 
+                    value={search} 
+                    onChange={handleChange}
+                />
+                <button
+                    id="submit"
+                    type="button"
+                    onClick={handleSubmit}
+                >
+                SEARCH
+                </button>
+            </form>
+            {allArticles}
+        </>
     )
 }
 
