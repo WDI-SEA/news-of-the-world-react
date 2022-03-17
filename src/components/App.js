badaeffc29a65ecf58dc0b0e6f98fb4fc66e01d6
Sol Youn <syoun93@gmail.com>
2022-03-17 01:57:08 -0700

    working api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
 import '../App.css';
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
+
 function App() {
+  
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_NEWS_API_KEY
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+    console.log("UseEffect is firing!")
+
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonData => {
+        console.log(jsonData.articles)
+      })
+  }, []) // Run one time
+
   return (
     <div className="App">
       <main>
