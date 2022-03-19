4755890ef763a7e85f58b50b5a9cee568c3cbee8
br1anlee <joonmolee3@gmail.com>
2022-03-18 21:33:59 -0700

    top news displays on landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
-
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
+
+const topNewsUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
+
+
+
+
 function App() {
+  const [topNewsList, setTopNewsList] = useState([])
+
+  useEffect(() => {
+    axios.get(topNewsUrl)
+      .then(response => {
+        console.log(response.data.articles)
+        setTopNewsList(response.data.articles)
+      })
+  }, [])
+
+    
   return (
     <div className="App">
       <main>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<Landing topNewsList={topNewsList}/>} />
           <Route path="/display" element={<Display />} />
         </Routes>
       </main>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-function Landing() {
+function Landing({topNewsList}) {
+    const newsList = topNewsList.map((news, i) => {
+        return (
+            <div topNewsList-key={i}>
+                <img src={news.urlToImage} alt="" />
+                <h1>{news.title}</h1>
+                 {news.author === null ? <h3>No Author</h3> : <h3>Written By: {news.author}</h3>}
+                <div>
+                    <p>{news.description}</p>
+                </div>
+            </div>
+        )
+    })
     return (
-        <h1>Landing Page</h1>
+        <>
+            <h1>Top News</h1>
+            {newsList}
+        </>
     )
 }
 
