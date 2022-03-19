f960316fc761603d3b37c3fa4dbb733aef597c38
br1anlee <joonmolee3@gmail.com>
2022-03-18 22:59:21 -0700

    reorganized codes

--- src/components/App.js
+++ src/components/App.js
@@ -6,29 +6,56 @@ import Display from './pages/Display';
 import Landing from './pages/Landing';
 
 
-const topNewsUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
-
-
 
 
 function App() {
   const [topNewsList, setTopNewsList] = useState([])
+  const [faves, setFaves] = useState([])
+  
+  const topNewsUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
 
+
+  // RECEIVING API DATA
   useEffect(() => {
     axios.get(topNewsUrl)
       .then(response => {
-        console.log(response.data.articles)
+        // console.log("This is the top news API",response.data.articles)
         setTopNewsList(response.data.articles)
       })
-  }, [])
+    }, [topNewsList])
 
+  // MAP THROUGH THE ARTICLES
+  const newsList = topNewsList.map((news, i) => {
+    return (
+      <div key={i} style={{
+        border: '2px solid black',
+        padding: "20px",
+        margin: "10px"
+      }}>
+        {news.urlToImage === null ? <h4>Image Unavailable</h4> : <img src={news.urlToImage} width={500} height='auto'/>}
+        <h2>{news.title}</h2>
+        <div>
+          <h5>{news.description}</h5>
+        </div>
+          {news.author === null || news.author === "" ? <h4>No Author</h4> : <p>Written By: {news.author}</p>}
+        <div>
+          <p>Click here for <a href={news.url} target="_blank">more</a> information about the article</p>
+        </div>
+        {/* <button onClick={() => setFaves([...faves, ])}>
+          Favorite this Article
+        </button> */}
+      </div>
+        
+
+    )
+  })
     
   return (
     <div className="App">
       <main>
         <Routes>
-          <Route path="/" element={<Landing topNewsList={topNewsList}/>} />
-          <Route path="/display" element={<Display topNewsList={topNewsList}/>} />
+          <Route path="/" element={<Landing newsList={newsList} setFaves={setFaves}faves={faves}/>} />
+          <Route path="/display" element={<Display  faves={faves}/>} />
         </Routes>
       </main>
     </div>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,21 +1,6 @@
 import React from "react";
 
-function Landing({topNewsList}) {
-    const newsList = topNewsList.map((news, i) => {
-        return (
-            <div>
-                <img src={news.urlToImage} alt="" />
-                <h1>{news.title}</h1>
-                <div>
-                    <h5>{news.description}</h5>
-                </div>
-                 {news.author === null || news.author === ""  ? <h3>No Author</h3> : <p>Written By: {news.author}</p> }
-                <div>
-                   <p>Click here for <a href={news.url} target="_blank">more</a> information about the article</p> 
-                </div>
-            </div>
-        )
-    })
+function Landing({newsList}) {
     return (
         <>
             <h1>Top News</h1>
