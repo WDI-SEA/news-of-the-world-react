cc8f1ef681802e2651bc5ab6c51ed7fd6cafd6a3
“ikarabag1” <irmakkarabag@gmail.com>
2022-03-22 18:18:53 +0300

    saved articles link added to dispaly and displaying faves work but needs fixing

--- src/components/pages/Display.jsx
+++ src/components/pages/Display.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useState } from 'react'
-
+import { Link } from "react-router-dom";
 // Displays all info about the article with the api id as indicated in the url params
 
 export default function Display({articles}) {
    
     // state setting for displayedArticle article displayed
     const [displayedArticle, setDisplayedArticle] = useState({})
-    
     const {idx} = useParams()
-   
+    const [savedArticles, setSavedArticles] = useState([])
+
+    const savedOne = (article) => {
+        if (!savedArticles.includes(article)) setSavedArticles([...savedArticles, article])
+    }
 
    return (
        <div> 
            <h1>News Article</h1>
+           <Link to={'/'}>Home</Link>
            <h3>
                Author: {articles[idx].author}
                </h3>
             <h4> 
                Title: {articles[idx].title}
                </h4>   
-               
+                 
+           <img alt="displayedarticleimage" src={articles[idx].urlToImage}/>
                <p>
                Description: {articles[idx].description}
                </p>
-            
-           <img alt="displayedarticleimage" src={articles[idx].urlToImage}/>
+            <button onClick={() => {savedOne(articles[idx])}}> save the article  </button>
        </div>
    )
 }
--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -3,20 +3,19 @@ import { Link } from "react-router-dom"
 
     // Add a functionality where the user can save articles (in case they want to read later). You can decide how you want the UX/UI for this part of it to go! Just make sure you have displaying a list on teh landing page of all the saved articles 
 
-    export default function Favorites({savedArticles}) {
-
-        const saved = savedArticles.map((savedOne, idx) => {
-            return (
-                <div
-                key={idx}>
-                    {savedOne.title}
-                </div>
+    export default function Favorites({articles}) {
+        
+        const savedArticles = articles.map((article, idx) => {
+            return ( 
+                <li key = {`saved-${idx}`}>
+                <Link to= {`/articles/${idx}`}> {article.title}</Link>
+                </li>
             )
         })
         return (
             <div>
             <h1>Saved Articles</h1>
-            <ul>{saved}</ul>
+            <ul>{savedArticles}</ul>
             <Link to={'/'}>
                 Home
             </Link>
