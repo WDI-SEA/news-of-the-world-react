4e256e31701b4e4f47adbbf2b0a1507b34f2bfbc
gracenarez333 <gracemnarez@gmail.com>
2022-06-17 14:14:07 -0700

    thers a bug but i had to stop

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Landing(props) {
+    let [search, setSearch] = useState('')
     const articleLinks = props.articles.map((article, idx) => {
         return <>
             <h3 key={article.title}>
@@ -9,8 +11,20 @@ export default function Landing(props) {
             </h3>
         </>
     })
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
     return (
         <>
+            <div>
+                <label htmlFor="article-search">Search the News:</label>
+                <input
+                    id="article-search"
+                    type="text"
+                    value={search}
+                    onChange={handleChange}
+                />
+            </div>
             {articleLinks}
         </>
     )
