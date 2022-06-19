f67dee56bc8b2d083f9a953b11928b1ab98417b2
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-19 14:39:05 -0600

    Style articles

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,19 +1,28 @@
 import React from "react"
 
 export const Article = ({ article }) => {
+  const [title, source] = article.title.split(" - ")
+  const publishedAt = new Date(article.publishedAt).toLocaleString("en-us", {
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  })
   return (
-    <a href={article.url}>
-      <article>
-        <header>
-          <p>
-            {article.author && <>{article.author} | </>}
-            {article.publishedAt}
+    <div className="w-1/2 max-w-md p-5 shadow rounded-md bg-blue-50 text-left">
+      <a href={article.url}>
+        <article className="">
+          <img
+            src={article.urlToImage}
+            alt={article.title}
+            className="m-auto my-5"
+          />
+          <p className="mb-2">
+            <span className="font-bold">{source}</span> | {publishedAt}
           </p>
-          <h2>{article.title}</h2>
-          <img src={article.urlToImage} alt={article.title} />
-          <p>{article.description}</p>
-        </header>
-      </article>
-    </a>
+          <h2 className="font-bold text-lg">{title}</h2>
+        </article>
+      </a>
+    </div>
   )
 }
