049c7a1adc3a38dbcf9cfb092182f75b17cfb581
marzo20 <myjune91@gmail.com>
2022-06-19 19:23:46 -0700

    apikey linked

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
+import axios from 'axios'
+
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
+import { useEffect } from 'react';
 
 function App() {
+
+  // axios
+  useEffect(()=> {
+    const fetchData = async () => {
+      try {
+        const url = `https://newsapi.org/v2/top-headlines/sources?apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+        const response = await axios.get(url)
+        console.log(response)
+      }catch(err){
+        console.warn('API ERROR', err)
+      }
+    }
+    fetchData()
+  },[]) //make sure this useEffect only runs the first time
+
   return (
     <div className="App">
       <main>
