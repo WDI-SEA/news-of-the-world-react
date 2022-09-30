b8160231b82ed0de922db1299e6b3f98e2b19c30
JasonXChen <jasonxchen888@gmail.com>
2022-09-30 19:47:38 -0400

    Fetch from News API and display titles

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,33 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function Landing() {
+    const [articles, setArticles] = useState([{title: "loading articles..."}]);
+    useEffect(() => {
+        const getArticles = async () => {
+            try {
+                const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
+                const articleData = await response.json();
+                setArticles(articleData.articles);
+            }
+            catch (error) {
+                console.warn(error);
+            }
+        }
+        getArticles();
+    }, []);
+    const articleComponents = articles.map((article, index) => {
+        return (
+            <div key={`article-${index}`}>
+                <h2>{article.title}</h2>
+            </div>
+        );
+    });
     return (
-        <h1>Landing Page</h1>
-    )
+        <div>
+            <h1>Landing Page</h1>
+            {articleComponents}
+        </div>
+    );
 }
 
 export default Landing;
