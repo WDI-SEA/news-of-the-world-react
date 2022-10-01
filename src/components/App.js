519ceb4bf58e2d335bd131186229be7381fa1908
prijacash <prijacash@gmail.com>
2022-10-01 15:35:23 -0700

    updated const vars

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
-function App() {
+export default function App() {
+
+  // search input
+  const [search, setSearch] = useState('')
+  // api data
+  const [data, setData] = useState()
+  // favorited articles
+  const [faves, setFaves] = useState([])
+
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-09-01&sortBy=publishedAt&apiKey=22c7e6648f81407b881453d91c44ec70'
+      fetch(url)
+        .then(response => response.json())
+        .then(jsonData => {
+
+        })
+  })  
+
+
   return (
     <div className="App">
       <main>
@@ -16,5 +35,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
