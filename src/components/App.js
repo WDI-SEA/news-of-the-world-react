2f6fe0989f84b4cb8b8557ac36f57c5c035c9ef5
elek8871 <elekmichelle@gmail.com>
2022-10-01 12:40:28 -0600

    updates app.js with state and url req

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import '../App.css';
-
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
 function App() {
+   // states
+   const [headlines, setHeadlines] = useState([])
+   const [search, setSearch] = useState(" ")
+
+   const api_key= process.env.REACT_APP_API_KEY
+   const url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`
+  //  const searchUrl =`https://newsapi.org/v2/everything?q=${userQuery}&apiKey=${App}`
+
+   useEffect(()=>{
+    async function getHeadlines(){
+      try{
+          const response = await axios.get(url)
+          // console.log(response.data.articles)
+          setHeadlines(response.data.articles)
+
+      }catch(error){
+          console.log(error)
+      }
+    }
+    getHeadlines()
+  }, [])
   return (
-    <div className="App">
-      <main>
+  
+      <div>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/display" element={<Display />} />
         </Routes>
-      </main>
-    </div>
+      </div>
   );
 }
 
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -2,16 +2,25 @@ import React from "react";
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
-function Landing(props) {
-    const [topheadlines, setTopHeadlinesews] = useState([])
+import Display from './Display'
 
-    useEffect(()=>{
-        const api_key= process.env.REACT_APP_API_KEY
-        const url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`
-        console.log(url)
-    })
+function Landing(props) {
+   
+
+   
     return (
-        <h1>Landing Page</h1>
+        <div>
+            <form onSubmit={props.handleSubmit}>
+                <input type ="text"
+                    name="search"
+                    // onChange={props.handleChange}
+                    // value={props.search} 
+                    />
+
+                <button type="submit">Search</button>
+            </form>
+            
+        </div>
     )
 }
 
