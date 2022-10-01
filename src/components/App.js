050ebbfc9cf272cc5240ee23658cd96230e96270
devin-lynch <devin.l.lynch@gmail.com>
2022-10-01 17:15:12 -0500

    Console logging API data

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,25 @@ import Display from './pages/Display';
 import Landing from './pages/Landing';
 
 import axios from 'axios'
+import { useEffect } from 'react';
 
 
 export default function App() {
 
   const api_key = process.env.REACT_APP_NEWS_API_KEY
-  console.log(api_key)
-  
+  // console.log(api_key)
+
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`
+
+  useEffect(() => {
+    console.log('useEffect is firing!')
+    axios.get(url)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(console.warn)
+  })
+
   return (
     <div className="App">
       <main>
