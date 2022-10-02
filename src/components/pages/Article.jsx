56dd2bb70bef2474e2451518cc6ec56b8a0a9601
devin-lynch <devin.l.lynch@gmail.com>
2022-10-02 15:47:41 -0500

    Display image on article

--- src/components/pages/Article.jsx
+++ src/components/pages/Article.jsx
@@ -1,7 +1,20 @@
-export default function Article() {
+import { useParams, Link } from 'react-router-dom'
+
+export default function Article(props) {
+    let { id } = useParams()
+    console.log(id)
+    const article = props.articles[id]
     return (
         <div>
             Article!
+            <h2>{article.title}</h2>
+            <div>
+                <img src={article.urlToImage} alt={article.title} />
+            </div>
+            {/* <p>{article.content}</p> */}
+            <p>{article.description}</p>
+            <p>{article.author ? article.author : null}</p>
+
         </div>
     )
 }
