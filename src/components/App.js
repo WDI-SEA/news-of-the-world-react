f17e48c7a4b4e2e70b3c99aee3e0e72962428db4
prijacash <prijacash@gmail.com>
2022-10-02 12:31:36 -0700

    updated browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import '../App.css';
+
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
@@ -17,13 +18,15 @@ export default function App() {
   // favorited articles
   const [faves, setFaves] = useState([])
 
+  const API_KEY = process.env.ENV_API_KEY
+
   const handleChange = (e) => {
     setSearch(e.target.value)
   }
 
   useEffect(() => {
     document.title = "News Search App"
-    const url = `https://newsapi.org/v2/everything?q=${toApi}&from=2022-09-01&sortBy=publishedAt&apiKey=22c7e6648f81407b881453d91c44ec70`
+    const url = `https://newsapi.org/v2/everything?q=${toApi}&from=2022-09-01&sortBy=publishedAt&apiKey=${API_KEY}`
       fetch(url)
         .then(response => response.json())
         .then(rData => {
@@ -41,19 +44,18 @@ export default function App() {
  
 
   return (
-    <div className="App">
-      <main>
-          <BrowserRouter>
-            <div>
-              <h1>WorldMedia News</h1>
-            </div>
-            <Routes>
-              <Route path="/" element={<Landing handleChange={handleChange} handleSubmit={handleSubmit} results={data.targetSearch} />} />
-              <Route path="/details/:id" element={<Display articles={data.targetSearch} />} />
-            </Routes>
-          </BrowserRouter>
 
-      </main>
-    </div>
+            <>
+              <div>
+                <h1>WorldMedia News</h1>
+              </div>
+
+              <Routes>
+                <Route path="/" element={<Landing handleChange={handleChange} handleSubmit={handleSubmit} results={data.targetSearch} />} />
+                <Route path="/details/:id" element={<Display articles={data.targetSearch} />} />
+              </Routes>  
+            </>
+  
+
   );
 }
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default function Landing(props) {
 
-    const results = props.results.map((article, i) => {
+    const results = props.results && props.results.map((article, i) => {
         return(
             <div
                 key={`article-${i}`}
@@ -22,6 +22,7 @@ export default function Landing(props) {
             <h1>Landing Page</h1>
             <form className="searchContainer" onSubmit={props.handleSubmit}>
                 <input type="text" name="search" className="searchBar" placeholder='SEARCH' onChange={props.handleChange} value={props.search} />
+                <button type='submit'>Submit</button>
             </form>
             {results}
         </div>
