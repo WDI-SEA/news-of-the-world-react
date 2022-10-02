58e5e98e3b5b7298687ac3b11776bbf81fb87303
prijacash <prijacash@gmail.com>
2022-10-01 17:20:18 -0700

    display completed

--- src/components/pages/Display.js
+++ src/components/pages/Display.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import { useParams } from 'react-router'
+// use params b/c its using an array. drill down for info
 
-function Display() {
+export default function Display(props) {
+    const params = useParams()
+    // use ternery
+    const content = props.articles ? props.articles[params.id] : 'Somethings off API or props aint working'
+    
     return (
-        <h1>Display Page</h1>
+        <div>
+            <h1>Display Page</h1>
+            <h1>{content.title}</h1>
+            <p>{content.content}</p>
+            
+            <div className="imageContainer">
+                <img className="image" src={content.urlToImage} alt={content.title} />
+            </div>
+            
+            <a href={content.url}>See Full Article</a>
+
+            <div>
+                <Link to='/'>Home</Link>
+            </div>
+
+        </div>
+
     )
 }
-
-export default Display;
