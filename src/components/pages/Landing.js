96d4d9327bf1d13799d6e432c07fe66dbde4c4fb
valerieyang00 <valerieyang00@gmail.com>
2022-10-02 13:57:58 -0700

    setSearch complete

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,14 +1,14 @@
-import React from "react";
+
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Landing() {
-    const [search, setSearch] = useState(null)
+    const [search, setSearch] = useState('')
     const [articles, setArticles] = useState([])
 
     useEffect(() => {
         const API_KEY = process.env.REACT_APP_NEWS_API_KEY
-        const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`
+        const url = `https://newsapi.org/v2/everything?q=null&apiKey=${API_KEY}`
         async function fetchNews() {
             try {
                 const response = await axios.get(url)
@@ -20,7 +20,22 @@ function Landing() {
             }
         }
         fetchNews()
-    })
+    },[])
+
+    const searchNews = async (e) => {
+        e.preventDefault()
+    
+        try {
+            const API_KEY = process.env.REACT_APP_NEWS_API_KEY
+            const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`
+            const response = await axios.get(url)
+            const articles = await response.data.articles
+            setArticles(articles)
+
+        }catch(err) {
+            console.warn(err)
+        }
+    }
 
     const articlesArr = articles.map((article, idx) => {
         return (
@@ -31,9 +46,25 @@ function Landing() {
         )
     })
 
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+       }
+
     return (
         <div>
             <h1>News</h1>
+            <form>
+            <label htmlFor='search'>Search Recent News:</label>
+            <input
+                id='search'
+                type='text'
+                placeholder='search news...'
+                value={search}
+                onChange={handleChange}
+            />
+            <button type='submit' onClick={searchNews}>Go</button>
+            </form>
+
             {articlesArr}
         </div>
 
