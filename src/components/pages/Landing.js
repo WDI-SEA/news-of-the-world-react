1ef5f590202db7678a6291cfcbb1aa4635e82b71
prijacash <prijacash@gmail.com>
2022-10-01 17:37:30 -0700

    worked on landing

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-function Landing() {
+export default function Landing(props) {
+
+    const results = props.results.map((article, i) => {
+        return(
+            <div
+                key={`article-${i}`}
+                className="link"
+            >
+                <Link to={`/details/${i}`} className='articleLinks'>
+                    <p>{article.title}</p>
+                </Link>
+            </div>
+        )
+    })
+
     return (
-        <h1>Landing Page</h1>
+        <div>
+            <h1>Landing Page</h1>
+            {results}
+        </div>
     )
 }
-
-export default Landing;
