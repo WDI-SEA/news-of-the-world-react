adac8a6c07ae878982f98f2d511361ad914d3d4d
prijacash <prijacash@gmail.com>
2022-10-01 17:01:14 -0700

    updated with apikey

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../App.css';
 
 import Display from './pages/Display';
@@ -10,27 +10,42 @@ export default function App() {
   // search input
   const [search, setSearch] = useState('')
   // api data
-  const [data, setData] = useState()
+  const [toApi, setApi] = useState('business')
+  // set data
+  const [data, setData] = useState({targetSearch: []})
   // favorited articles
   const [faves, setFaves] = useState([])
 
   useEffect(() => {
-    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-09-01&sortBy=publishedAt&apiKey=22c7e6648f81407b881453d91c44ec70'
+    document.title = "News Search App"
+    const url = `https://newsapi.org/v2/everything?q=${toApi}&from=2022-09-01&sortBy=publishedAt&apiKey=22c7e6648f81407b881453d91c44ec70`
       fetch(url)
         .then(response => response.json())
-        .then(jsonData => {
-
+        .then(rData => {
+            setData({targetSearch: rData.articles})
+            console.log(rData)
         })
-  })  
+      }, [toApi])  
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setApi(search)
+  }
+ 
 
   return (
     <div className="App">
       <main>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/display/:id" element={<Display />} />
-        </Routes>
+        <BrowserRouter>
+          <div>
+            <h1>WorldMedia News</h1>
+          </div>
+
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/display/:id" element={<Display />} />
+          </Routes>
+        </BrowserRouter>
       </main>
     </div>
   );
