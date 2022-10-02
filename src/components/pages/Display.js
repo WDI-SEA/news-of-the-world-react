52738da88ef71f6e4e72e569a32759d4dd2eff2f
valerieyang00 <valerieyang00@gmail.com>
2022-10-02 15:17:58 -0700

    up to part 3 done with searchbar working on both routes

--- src/components/pages/Display.js
+++ src/components/pages/Display.js
@@ -1,8 +1,24 @@
-import React from "react";
+import { useParams } from 'react-router-dom'
+import SearchBar from '../partials/SearchBar'
 
-function Display() {
+function Display(props) {
+    let { id } = useParams()
+    const article = props.articles[id]
+
     return (
-        <h1>Display Page</h1>
+        <div>
+            <h1>News</h1>
+            <SearchBar search={props.search} handleChange={props.handleChange} setArticles={props.setArticles}/>
+            <ul>
+            <h3>{article.title}</h3>
+            <p>Source: {article.source.name} Date: {article.publishedAt}</p>
+            <img src={article.urlToImage} alt={article.title} />
+            <p>{article.description}</p>
+            <p>{article.content}</p>
+            <a target='_blank' rel="noreferrer" href={article.url}>Click for Full Article</a>
+            </ul>
+           </div>
+
     )
 }
 
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,70 +1,22 @@
+import { Link } from 'react-router-dom';
+import SearchBar from '../partials/SearchBar'
 
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+function Landing(props) {
 
-function Landing() {
-    const [search, setSearch] = useState('')
-    const [articles, setArticles] = useState([])
-
-    useEffect(() => {
-        const API_KEY = process.env.REACT_APP_NEWS_API_KEY
-        const url = `https://newsapi.org/v2/everything?q=null&apiKey=${API_KEY}`
-        async function fetchNews() {
-            try {
-                const response = await axios.get(url)
-                const articles = await response.data.articles
-                setArticles(articles)
-
-            } catch (err) {
-                console.warn(err)
-            }
-        }
-        fetchNews()
-    },[])
-
-    const searchNews = async (e) => {
-        e.preventDefault()
-    
-        try {
-            const API_KEY = process.env.REACT_APP_NEWS_API_KEY
-            const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`
-            const response = await axios.get(url)
-            const articles = await response.data.articles
-            setArticles(articles)
-
-        }catch(err) {
-            console.warn(err)
-        }
-    }
-
-    const articlesArr = articles.map((article, idx) => {
+     const articlesArr = props.articles.map((article, idx) => {
         return (
         <ul key={`article-${idx}`}>
-            <h3>{article.title}</h3>
+            <h3><Link to={`/articles/${idx}`}>{article.title}</Link></h3>
             <p>{article.content}</p>
         </ul>
         )
     })
 
-    const handleChange = (e) => {
-        setSearch(e.target.value)
-       }
 
     return (
         <div>
             <h1>News</h1>
-            <form>
-            <label htmlFor='search'>Search Recent News:</label>
-            <input
-                id='search'
-                type='text'
-                placeholder='search news...'
-                value={search}
-                onChange={handleChange}
-            />
-            <button type='submit' onClick={searchNews}>Go</button>
-            </form>
-
+            <SearchBar search={props.search} handleChange={props.handleChange} setArticles={props.setArticles}/>
             {articlesArr}
         </div>
 
