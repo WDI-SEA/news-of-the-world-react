88cbda0ea5c8b1b57dc55899119a3fa0a286d4fb
tatebonham <tatebonham@gmail.com>
2022-10-03 10:29:17 -0700

    fixed errors with deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,48 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
+import axios from 'axios';
 
 import Display from './pages/Display';
 import Landing from './pages/Landing';
 
 function App() {
+  const [apiResponse, setApiResponse]=useState([])
+  const [inputValue, setInputValue]=useState('')
+  const [search, setSearch]=useState('programming')
+  const [readLater, setReadLater]=useState([])
+  console.log(process.env.REACT_APP_API_KEY)
+  useEffect(()=>{
+    console.log(process.env.REACT_APP_API_KEY)
+    //https://newsapi.org/v2/everything?q=tesla&from=2022-09-03&sortBy=publishedAt&apiKey=86dd72e0766741dd85178462247aeb55
+    const getNews = async ()=>{
+      try{
+        const url = `https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
+        const response = await axios.get(url)
+        setApiResponse(response.data.articles)
+      }catch(err){
+        console.warn(err)
+      }
+    }
+    getNews()
+  },[search])
+
   return (
     <div className="App">
       <main>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/display" element={<Display />} />
+          <Route path="/" element={
+                <Landing 
+                apiResponse={apiResponse} 
+                inputValue={inputValue} 
+                setInputValue={setInputValue} 
+                setSearch={setSearch} 
+                readLater={readLater}
+                setReadLater={setReadLater}
+                />
+              } 
+          />
+          <Route path="/display/:id" element={<Display apiResponse={apiResponse} />} />
         </Routes>
       </main>
     </div>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,8 +1,50 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
-function Landing() {
+function Landing(props) {
+    const articles = props.apiResponse.map((article, i)=>{
+        return(
+            <div key={`article${i}`}>
+                <Link  to={`/display/${i}`} > 
+                    <h2>{article.title}</h2>
+                    <p>{article.desciption}</p>
+                    <p>by: {article.author}</p>
+                </Link>
+                <div>
+                    <button onClick={()=>props.setReadLater([...props.readLater, {id: i, article}])} >read later</button>
+                </div>
+
+            </div>
+        )
+    })
+    const readLaters = props.readLater.map(readLater=>{
+        return(
+            <li key={`readLater${readLater.id}`} >
+                <Link to={`/display/${readLater.id}`} >{readLater.article.title}</Link>
+            </li>
+        )
+    })
+    const handleSubmit = e =>{
+        e.preventDefault()
+        props.setSearch(props.inputValue)
+    }
     return (
-        <h1>Landing Page</h1>
+        <>
+            <h1>News Search</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="input" >Search:</label>
+                <input type='text' value={props.inputValue} onChange={e => props.setInputValue(e.target.value)} />
+                <button type='submit' >Search</button>
+            </form>
+            Read Later:
+            <ul>
+                {readLaters}
+            </ul>
+
+            Articles:
+            {articles}
+        </>
     )
 }
 
