83701c0b23d67616fba1e1f95cff45b454773471
brandon-w1205 <bwu1205@gmail.com>
2022-10-03 22:20:07 -0700

    adds correct form submission for search function

--- src/components/App.js
+++ src/components/App.js
@@ -21,29 +21,32 @@ function App() {
     setState(e.target.value)
   }
 
-      
+
   const handleClick = (e) => {
     setSearch(e)
   }
 
-  const handleRequest = async () => {
-    try {
-      const response = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${KEY.api_key}`)
-      setNews(response.data.articles)
-    } catch(err) {
-      console.warn(err)
+
+
+
+
+  useEffect(() => {
+    const handleRequest = async () => {
+      try {
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${KEY.api_key}`)
+        setNews(response.data.articles)
+      } catch (err) {
+        console.warn(err)
+      }
     }
-  }
-
-  
-
-  useEffect (() => {
     handleRequest()
-    
   }, [search])
 
+
+
+
   const handleFaves = e => {
-    if(!faves.includes(e.target.value)){
+    if (!faves.includes(e.target.value)) {
       setFaves([...faves, e.target.value])
     }
     e.stopPropagation()
@@ -59,9 +62,19 @@ function App() {
       <main>
         <Header />
         <Routes>
-          <Route path="/" element={<Landing search={search} state={state} handleChange={handleChange} handleClick={handleClick} news={news} />} />
-          <Route path="/articles/:articleId" element={<Display news={news} handleFaves={handleFaves}/>} />
-          <Route path="/favorites" element={<Favorite news={news} faves={faves} handleRemove={handleRemove}/>} />
+          <Route path="/" element={
+            <Landing 
+              search={search} 
+              state={state} 
+              handleChange={handleChange} 
+              handleClick={handleClick}
+              setSearch={setSearch} 
+              news={news} 
+            />
+          } 
+          />
+          <Route path="/articles/:articleId" element={<Display news={news} handleFaves={handleFaves} />} />
+          <Route path="/favorites" element={<Favorite news={news} faves={faves} handleRemove={handleRemove} />} />
         </Routes>
       </main>
     </div>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,34 +1,41 @@
 import { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Landing(props) {
 
     const [input, setInput] = useState('')
     let results = ""
-    
-    if(props.search !== '') {
+
+    if (props.search !== '') {
         results = props.news.map((result, i) => {
             return (
                 <div>
-                    <h2><Link to={`/articles/${i}`} style={{color:'white'}}>{result.title}</Link></h2>
+                    <h2><Link to={`/articles/${i}`} style={{ color: 'white' }}>{result.title}</Link></h2>
                 </div>
             )
         })
     }
-    
 
 
-    
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.setSearch(input)
+    }
+
     return (
         <>
             <h1>Landing Page</h1>
-            <p>
-                Search:
-                <input type="text" id="search" onChange={e => setInput(e.target.value)} />
-                <button type='submit' id="search" onClick={() => props.handleClick(input)}>Submit</button>
-            </p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="input">Search:</label>
+                <input
+                    type="text"
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                />
+                <button type='submit'>Search</button>
+            </form>
             {results}
-            
+
         </>
     )
 }
