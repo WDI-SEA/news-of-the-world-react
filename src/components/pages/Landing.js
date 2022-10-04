12d022038002fba3a9fb46c85a4b126e7ab24c94
Moodybleu <meganbenn27@icloud.com>
2022-10-03 17:31:58 -0700

    Finishes deliverable

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,8 +1,61 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-function Landing() {
+function Landing(props) {
+    const articles = props.apiResponse.map((article, i) => {
+        return (
+            <div key={`article${i}`}>
+                <Link to={`/display/${i}`}>
+                    <h3>{article.title}</h3>
+
+                    <p>{article.description}</p>
+
+                    <h4>by: {article.author}</h4>
+                </Link>
+
+                <div>
+                    <button onClick={() => props.setReadLater([...props.readLater, { id: i, article }])}>read later</button>
+                </div>
+            </div>
+        )
+    })
+    const readLaters = props.readLater.map(readLater => {
+        return(
+            <li key={`readLater${readLater.id}`}>
+                <Link to={`/display/${readLater.id}`}>{readLater.article.title}</Link>
+            </li>
+        )
+    })
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.setSearch(props.inputValue)
+    }
+
     return (
-        <h1>Landing Page</h1>
+        <>
+            <h1>News Search</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="input">Search:</label>
+                <input 
+                    type='text'
+                    value={props.inputValue}
+                    onChange={e => props.setInputValue(e.target.value)}
+                />
+
+                <button type='submit'>Search</button>
+            </form>
+
+            <h2>Read Later:</h2>
+
+            <ul>
+                {readLaters}
+            </ul>
+
+            <h2>Articles:</h2>
+            
+            {articles}
+        </>
     )
 }
 
